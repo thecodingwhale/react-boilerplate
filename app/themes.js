e23ac6397af45121eec7445d3180cb260f03c0b9
Aldren Terante <aldren.terante@gmail.com>
2016-12-14 15:38:29 +0800

    add baseColor and themes

---
+++ app/themes.js
@@ -0,0 +1,12 @@
+import { baseColor } from './baseColor';
+
+export const themes = {
+  dark: {
+    bgColor: baseColor.primary,
+    fontColor: '#ffffff',
+  },
+  light: {
+    bgColor: '#ffffff',
+    fontColor: baseColor.primary,
+  },
+};
